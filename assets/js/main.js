739178a0d9ae7068b55400bbc4e9fcf76844da0c
MSayib <sayib.roziq2@gmail.com>
2023-11-30 23:15:03 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    $("#slider-hero").owlCarousel({
+        nav: true,
+        items: 1,
+        navText: [
+            "<li class='fas fa-angle-left'><i>",
+            "<li class='fas fa-angle-right'><i>"
+        ],
+        navContainer: "#slider-hero-nav",
+    });
+
+
+
+
+
+
+
+});
